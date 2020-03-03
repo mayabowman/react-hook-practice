4236d996deb96f59b1218ebcb96db8b0382a2d0b
Maya Bowman <mayashady1@gmail.com>
2020-03-03 14:16:36 -0500

    practiced hooks using arrays

--- src/App.js
+++ src/App.js
@@ -3,9 +3,20 @@ import React, { useState } from 'react';
 function App({ text, maxLength}) {
   const [count, setCount] = useState(0);
   const [hidden, setHidden] = useState(true);
+  const [items, setItems] = useState([]);
 
   if (text.length <= maxLength) {
     return <span>{text}</span>
+  }
+
+  function addItem() {
+    setItems([
+      ...items,
+      {
+        id: items.length,
+        value: Math.random() * 100
+      }
+    ])
   }
 
 
@@ -26,6 +37,14 @@ function App({ text, maxLength}) {
           <div><a onClick={() => setHidden(true)}>show less</a></div>
         )}
       </span>
+      <div className='random-number'>
+        <button onClick={addItem}>Add a Number</button>
+        <ul>
+          {items.map(item => (
+            <li key={item.id}>{item.value}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
