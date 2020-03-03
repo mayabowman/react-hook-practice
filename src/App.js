7c7cfb50f08ef8387f9dfb962de3c6844eb1393d
Maya Bowman <mayashady1@gmail.com>
2020-03-03 13:56:26 -0500

    adding to practice

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 
 function App({ text, maxLength}) {
-  // const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
   const [hidden, setHidden] = useState(true);
 
   if (text.length <= maxLength) {
@@ -10,20 +10,23 @@ function App({ text, maxLength}) {
 
 
   return (
-    // <div className="App">
-    //   <p>You clicked {count} times</p>
-    //   <button onClick={() => setCount(count + 1)}>
-    //     Click Me
-    //   </button>
-    // </div>
-    <span>
-      {hidden ? `${text.substr(0, maxLength).trim()}...` : text}
-      {hidden ? (
-        <a onClick={() => setHidden(false)}>show more</a>
-      ) : (
-        <a onClick={() => setHidden(true)}>show less</a>
-      )}
-    </span>
+    <div>
+      <div className="App">
+        <p>You clicked {count} times</p>
+        <button onClick={() => setCount(count + 1)}>
+          Click Me
+        </button>
+      </div>
+      <span>
+        <h2>Let's see it! Or not...</h2>
+        {hidden ? `${text.substr(0, maxLength).trim()}...` : text}
+        {hidden ? (
+          <div><a onClick={() => setHidden(false)}>show more</a></div>
+        ) : (
+          <div><a onClick={() => setHidden(true)}>show less</a></div>
+        )}
+      </span>
+    </div>
   );
 }
 
