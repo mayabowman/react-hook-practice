01e0056af6197dfca025056000f3a843a7f9b99b
Maya Bowman <mayashady1@gmail.com>
2020-03-03 13:44:52 -0500

    practicing useState hook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+function App({ text, maxLength}) {
+  // const [count, setCount] = useState(0);
+  const [hidden, setHidden] = useState(true);
+
+  if (text.length <= maxLength) {
+    return <span>{text}</span>
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    //   <p>You clicked {count} times</p>
+    //   <button onClick={() => setCount(count + 1)}>
+    //     Click Me
+    //   </button>
+    // </div>
+    <span>
+      {hidden ? `${text.substr(0, maxLength).trim()}...` : text}
+      {hidden ? (
+        <a onClick={() => setHidden(false)}>show more</a>
+      ) : (
+        <a onClick={() => setHidden(true)}>show less</a>
+      )}
+    </span>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <App
+    text={`And the stone walls of Harmony Hall bear witness
+    Anybody with a worried mind could never forgive the sight
+    Of wicked sakes inside a place you thought was dignified
+    I don't wanna live like this, but I don't wanna die`}
+    maxLength={35}
+  />,
+  document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
